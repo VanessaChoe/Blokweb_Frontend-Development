c5eb8500dc96937ac76a7137d8a68877b68f162e
VanessaChoe <90140769+VanessaChoe@users.noreply.github.com>
2021-11-11 16:35:18 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,21 @@
 /* https://codepen.io/shooft/pen/dyRVVRr */
-var deButton = document.querySelector("nav img");
+ var deButton = document.querySelector("nav img");
 
-deButton.addEventListener("click", toggleMenu);
+ deButton.addEventListener("click", toggleMenu);
 
-function toggleMenu(event) {
-  deNav = event.target.parentNode;
-  deNav.classList.toggle("toonMenu");
-}
+ function toggleMenu(event) {
+   deNav = event.target.parentNode;
+   deNav.classList.toggle("toonMenu");
+ }
+
+//  https://benfrain.com/converting-divs-into-accessible-pseudo-buttons//////
+
+ var deButton = document.querySelector("nav img");
+
+ deButton.addEventListener("keydown", e => {
+  if (e.key === "Enter" || e.key === "Spacebar") {
+    deNav = event.target.parentNode;
+    deNav.classList.toggle("toonMenu");
+  }
+});
+
